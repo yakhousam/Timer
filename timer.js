68c0b295024882d328129e11a869d4486f00536d
yakhousam <42283612+yakhousam@users.noreply.github.com>
2018-12-30 19:14:24 +0100

    set timer with mouse wheel, animation, input file

--- timer.js
+++ timer.js
@@ -1,58 +1,132 @@
 
-let timer = document.querySelector('#time');
-let clock = document.querySelector('.clock');
-let start = document.querySelector('.start');
-let music = document.querySelector('.sound audio');
-let second = 3;
+const hr = document.querySelector('.hour');
+const mn = document.querySelector('.minute');
+const sd = document.querySelector('.second');
+const clock = document.querySelector('.clock');
+const clockContainer = document.querySelector('.clock-container');
+const btn_start = document.querySelector('.btn-start');
+const music = document.querySelector('.sound audio');
+const input_musicFile = document.querySelector('.input-musicUpload');
+const btn_reset = document.querySelector('.btn-reset');
+
+let second = 0;
 let stratTimerEvent='';
 
-timer.addEventListener('input', ()=>{
-    second = timer.value * 60;
-    clock.textContent = secondToTime(second);
+if(localStorage.audioFile){
+    music.setAttribute('src', localStorage.audioFile);
+}
+
+input_musicFile.addEventListener('change', (e)=>{
+    let file = e.target.files[0].name;
+    console.log(file);
+    music.setAttribute('src', file);
+    localStorage.audioFile = file;
+})
+
+
+
+hr.addEventListener('wheel', (e)=>{
+  
+
+    let hour = Number(hr.textContent);
+    e.deltaY < 0 ? hour += 1 : hour > 1 ? hour -= 1 : hour = 24;
+    hour = hour < 25 ? hour : 0;
+    hr.textContent = hour > 9 ? hour : '0' + hour;
+
     clearTimeout(stratTimerEvent);
 
 })
 
+mn.addEventListener('wheel', (e)=>{
+   
+    let minute = Number(mn.textContent);
+    e.deltaY < 0 ? minute += 1 : minute > 1 ? minute -= 1 : minute = 59;
+    minute = minute < 60 ? minute : 0;
+    mn.textContent = minute > 9 ? minute : '0' + minute;
 
-start.addEventListener('click', startTimer);
+    clearTimeout(stratTimerEvent);
+
+})
+
+sd.addEventListener('wheel', (e)=>{
+   
+    let s = Number(sd.textContent);
+    e.deltaY < 0 ? s += 1 : s > 1 ? s -= 1 : s = 59;
+    s = s < 60 ? s : 0;
+    sd.textContent = s > 9 ? s : '0' + s;
+
+    clearTimeout(stratTimerEvent);
+
+})
+
+btn_start.addEventListener('click', ()=>{
+    second = Number(hr.textContent)* 3600 + Number(mn.textContent) *60 + Number(sd.textContent)
+   
+    startTimer();
+});
+btn_reset.addEventListener('click', resetTimer);
 
 function startTimer(){
-    second -= 1;
-    clock.textContent = secondToTime(second);
-    
-    timer.valueAsNumber = Math.floor(second/60);
+    clock.classList.add('timerStarted');
+    document.querySelector('.pendulum-1').classList.add('pendulum-animate');
+    document.querySelector('.pendulum-2').classList.add('pendulum-2-animate');
 
+    clearTimeout(stratTimerEvent);
+    second -= 1;
+    let time = secondToTime(second);
+    hr.textContent = time[0] + time[1];
+    mn.textContent = time[3] + time[4];
+    sd.textContent = time[6] + time[7];
+    
     stratTimerEvent = setTimeout(startTimer, 1000);
-    if(second<1){
+    if(second == 0){
         clearTimeout(stratTimerEvent);
+        elapsedTime();
         music.play();
         console.log('start play music') 
     }
 }
 
-function secondToTime(second){
+function elapsedTime(){
+    clearTimeout(stratTimerEvent);
+    clock.classList.remove('timerStarted');
+    let time = secondToTime(second);
+    hr.textContent = time[0] + time[1];
+    mn.textContent = time[3] + time[4];
+    sd.textContent = time[6] + time[7];
+    second += 1 ;
+    stratTimerEvent = setTimeout(elapsedTime, 1000);
+    clock.classList.add('timeElapsed');
+    clock.parentElement.classList.add('timeElapsed');
+}
+
+function formatNumber(num){
+    return num < 10 ? "0" + num : num;
+}
+
+function secondToTime(second){   
     if(second<1){
         return "00:00:00";
     }
     let s = second;
-    let m = 0;
-    let h = 0;
-
-    h = Math.floor(s / 3600);
-    if(h<10){
-        h = "0"+h;
-    }
-    s -= h * 3600;
+    let h = Math.floor(s / 3600);
+    let m = Math.floor(s / 60);
     
-    m = Math.floor(s / 60);
-    if(m<10){
-        m = "0"+m;
-    }    
-    s -= m * 60;
-    if(s<10){
-        s = "0"+s;
-    }
-
-    return h+":"+m+":"+s ;
+    s -= h * 3600 + m * 60;
+    
+    return formatNumber(h)+":"+formatNumber(m)+":"+formatNumber(s) ;
 }
 
+function resetTimer(){ 
+    document.querySelector('.pendulum-1').classList.remove('pendulum-animate');
+    document.querySelector('.pendulum-2').classList.remove('pendulum-2-animate');
+    clearTimeout(stratTimerEvent);
+    clock.classList.remove("timeElapsed");
+    clock.classList.remove('timerStarted');
+    clock.parentElement.classList.remove('timeElapsed');
+    hr.textContent = '00';
+    mn.textContent = '00';
+    sd.textContent = '00';
+    second = 0;
+    music.load();
+}
